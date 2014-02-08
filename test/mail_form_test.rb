ac1ae9ab1f10cbf726cc1958876e4b5f0857966f
yachibit <y.yachix@gmail.com>
2014-02-08 15:30:28 +0900

    Define clear_attribute method

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -2,6 +2,20 @@ require 'test_helper'
 require "fixtures/sample_mail"
 
 class MailFormTest < ActiveSupport::TestCase
+  test "sample mail can clear attributes using clear_ prefix" do
+    sample = SampleMail.new
+
+    sample.name  = "User"
+    sample.email = "user@example.com"
+    assert_equal "User", sample.name
+    assert_equal "user@example.com", sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
+  end
+
   test "sample mail has name and email as attributes" do
     sample = SampleMail.new
     sample.name = "User"
