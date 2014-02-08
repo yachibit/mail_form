a78e138c75d1fb7c70eb42bb92d4848644d5e9b9
yachibit <y.yachix@gmail.com>
2014-02-08 18:37:08 +0900

    Comply with Active Model API

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,12 +1,21 @@
 module MailForm
   class Base
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
     include ActiveModel::AttributeMethods
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
 
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+    end
+
+    def persisted?
+      false
     end
 
     protected
